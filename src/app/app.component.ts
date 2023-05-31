ec948a9070ccc59a264f44c99b99c4c14c000d11
Mahedi Hasan Niloy <niloy.oct@gmail.com>
2023-05-31 12:44:12 +0600

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBasics';
+  title = 'AngularApp';
+  message = "Angular is a Javascript framework widely use for Web application"
 }
